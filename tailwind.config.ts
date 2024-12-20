00281f3de7e8dcca4949a0d8db7c2339792ab623
yachaechae <wpql1207@gmail.com>
2024-12-20 16:47:11 +0900

    feat: 추가 패키지 설치 및 tailwind 추가 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,23 @@
 import type { Config } from "tailwindcss";
+import {nextui} from "@nextui-org/react";
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        navy: "#232537",
+        point:"#E6941C"
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 } satisfies Config;
