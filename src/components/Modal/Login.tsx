f55b3118876d9ce48d738cfa068c5e1282d1eadb
yachaechae <wpql1207@gmail.com>
2024-12-20 17:02:36 +0900

    feat: Navbar 및 로그인 모달 컴포넌트화

---
+++ src/components/Modal/Login.tsx
@@ -0,0 +1,73 @@
+// LoginModal.jsx
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Input,
+  Checkbox,
+  Button,
+  Link,
+} from '@nextui-org/react';
+import { MailIcon, LockFilledIcon } from '@nextui-org/shared-icons';
+
+interface LoginModalProps {
+  isOpen: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+function LoginModal({ isOpen, onOpenChange }: LoginModalProps) {
+  return (
+    <Modal isOpen={isOpen} placement="top-center" onOpenChange={onOpenChange}>
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
+            <ModalBody>
+              <Input
+                endContent={
+                  <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                }
+                label="Email"
+                placeholder="Enter your email"
+                variant="bordered"
+              />
+              <Input
+                endContent={
+                  <LockFilledIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                }
+                label="Password"
+                placeholder="Enter your password"
+                type="password"
+                variant="bordered"
+              />
+              <div className="flex py-2 px-1 justify-between">
+                <Checkbox
+                  classNames={{
+                    label: 'text-small',
+                  }}
+                >
+                  Remember me
+                </Checkbox>
+                <Link color="primary" href="#" size="sm">
+                  Forgot password?
+                </Link>
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="danger" variant="flat" onPress={onClose}>
+                Close
+              </Button>
+              <Button color="primary" onPress={onClose}>
+                Sign in
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export default LoginModal;
---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import {
+  Button,
+  Link,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  useDisclosure,
+} from '@nextui-org/react';
+import ThemeToggle from '@/components/Theme/ThemeSwitcher';
+import LoginModal from '@/components/Modal/Login';
+
+function NavBar() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <>
+      <Navbar>
+        <NavbarBrand>
+          <p className="font-bold text-inherit">E-SPORT AUCTION</p>
+        </NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarItem>
+            <Link color="foreground" href="/create">
+              경매등록
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              경매등록
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              내 경매
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem className="hidden lg:flex">
+            {/* 로그인 클릭 시 모달 열기 */}
+            <Link href="#" onClick={onOpen}>
+              Login
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Button
+              as={Link}
+              className="bg-navy text-white"
+              href="#"
+              variant="flat"
+            >
+              Sign Up
+            </Button>
+          </NavbarItem>
+          <NavbarItem>
+            <ThemeToggle />
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+
+      <LoginModal isOpen={isOpen} onOpenChange={onOpenChange} />
+    </>
+  );
+}
+
+export default NavBar;
