f8f11f2815cfc3142e9bac08f8c7a89f2798d78e
yachaechae <wpql1207@gmail.com>
2025-03-12 02:56:15 +0900

    feat: 플레이어의 티어정보에 맞는 프로필 이미지 제작 및 리스트화, 이미지 추가

---
+++ src/components/PlayProfile/ProfileImg.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Image from 'next/image';
+
+export default function ProfileImg({
+  highestTier,
+  profileImgUrl,
+}: {
+  highestTier: string;
+  profileImgUrl: string;
+}) {
+  return (
+    <div className="relative w-44 h-48 py-8">
+      <Image
+        src={`/img/Wings/${highestTier}.png`}
+        alt="큰 이미지"
+        fill
+        className="absolute object-cover z-20"
+      />
+      <div className="absolute bottom-2 left-0 right-0 translate-y-[-2rem] translate-x-[3.5rem]">
+        <div className="relative w-16 h-16">
+          <Image
+            src={`${profileImgUrl}`}
+            alt="작은 이미지"
+            fill
+            className="object-contain z-20 rounded-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
