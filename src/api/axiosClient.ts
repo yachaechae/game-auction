86e260653a06ff7d1b280bc76d89216b55ad8a2e
yachaechae <wpql1207@gmail.com>
2025-03-12 02:55:20 +0900

    feat: axios 및 useQuery 를 사용하여 라이엇에서 제공하는 챔피언 데이터 연결

---
+++ src/api/axiosClient.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const baseURL = process.env.NEXT_PUBLIC_RIOT_URL;
+
+const instance = axios.create({
+  baseURL: baseURL,
+  timeout: 10000,
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    console.log('요청 시작:', config);
+    return config;
+  },
+  (error) => {
+    console.error('요청 에러:', error);
+    return Promise.reject(error);
+  },
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    console.log('응답 성공:', response);
+    return response;
+  },
+  (error) => {
+    console.error('응답 에러:', error);
+
+    if (error.response?.status === 401) {
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default instance;
