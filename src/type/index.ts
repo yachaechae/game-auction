394b2f8cb345ab098e5c622448db2ba084eeeed5
yachaechae <wpql1207@gmail.com>
2025-03-12 22:28:42 +0900

    feat: 데이터 타입 추가

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,8 +1,11 @@
-export interface TierImgType {
+export interface ImageType {
   id: number;
   name: string;
   src: string;
 }
+
+export type TierImgType = ImageType;
+export type PositionImgType = ImageType;
 
 export interface ImgSelectBoxProps {
   data: TierImgType[];
@@ -13,8 +16,58 @@ export interface ImgSelectBoxProps {
   placeholder: string;
 }
 
+export interface AutocompleteProps {
+  selectedHeroes: Champion[];
+  onChange: (value: Champion[]) => void;
+  label: string;
+  placeholder: string;
+}
+
 export interface PostCreateData {
   teamCount: number;
   minTier: TierImgType;
   leaderPoints: number;
+}
+
+export interface Champion {
+  version: string;
+  id: string;
+  name: string;
+  image: {
+    full: string;
+  };
+}
+
+export type ChampionData = {
+  [key: string]: Champion;
+};
+
+export interface UserDataType {
+  id: number;
+  userId: string;
+  inGameName: string;
+  highestTier: string;
+  primaryLane: string;
+  secondaryLane: string;
+  mostPlayedHeroes: string;
+  profileImgUrl: string | undefined | null;
+  selfIntroduction: string;
+  createdDate: string;
+  updatedDate: string;
+}
+
+export interface RegisterFormData {
+  inGameName: string;
+  highestTier: TierImgType;
+  primaryLane: string;
+  secondaryLane: string | null;
+  mostPlayedHeroes: Champion[];
+  profileImgUrl: string | null;
+  selfIntroduction: string;
+}
+
+export interface CustomRadioProps {
+  imgSrc: string;
+  value: string;
+  children: React.ReactNode;
 }
