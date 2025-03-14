0bbd5169779397bedc2e466bed8cf680987f8662
yachaechae <wpql1207@gmail.com>
2025-03-15 03:22:44 +0900

    feat: 경매장 내부 로직 미연결로 인한 주석

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className="h-full flex flex-col row-start-2 items-center justify-around sm:items-center">
         <CustomButton targetUrl={'/create'} pageName={'경매 등록'} />
         <CustomButton targetUrl={'/register'} pageName={'프로필 등록'} />
-        <CustomButton targetUrl={'/join'} pageName={'경매 참여'} />
+        {/*<CustomButton targetUrl={'/join'} pageName={'경매 참여'} />*/}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
