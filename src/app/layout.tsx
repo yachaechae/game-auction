30fee35c3991d5503ad554239bb3ae8b3c35183d
yachaechae <wpql1207@gmail.com>
2025-03-15 01:33:55 +0900

    fix: 타입에러 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { NextProviders, ReactQueryProvider } from '../providers';
 import NavBar from '@/components/NavBar/NavBar';
+import { ReactNode } from 'react';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -22,7 +23,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <ReactQueryProvider>
